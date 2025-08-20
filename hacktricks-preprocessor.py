d13c1d49708faf4fe262535246946ca320b99dd4
Translator <github-actions@github.com>
2025-08-20 19:22:12 +0000

    Translated ['src/mobile-pentesting/android-app-pentesting/intent-injecti

--- hacktricks-preprocessor.py
+++ hacktricks-preprocessor.py
@@ -50,6 +50,7 @@ def ref(matchobj):
                 pass #nDont stop on broken link
     else:
         try:
+            href = href.replace("`", "") # Prevent hrefs like: ../../generic-methodologies-and-resources/pentesting-network/`spoofing-llmnr-nbt-ns-mdns-dns-and-wpad-and-relay-attacks.md`
             if href.endswith("/"):
                 href = href+"README.md" # Fix if ref points to a folder
             if "#" in  href:
@@ -61,9 +62,10 @@ def ref(matchobj):
                 logger.debug(f'Recursive title search result: {chapter["name"]}')
                 title = chapter['name']
         except Exception as e:
+            dir = path.dirname(current_chapter['source_path'])
+            rel_path = path.normpath(path.join(dir,href))
             try:
-                dir = path.dirname(current_chapter['source_path'])
-                logger.debug(f'Error getting chapter title: {href} trying with relative path {path.normpath(path.join(dir,href))}')
+                logger.debug(f'Not found chapter title from: {href} -- trying with relative path {rel_path}')
                 if "#" in  href:
                     chapter, _path = findtitle(path.normpath(path.join(dir,href.split('#')[0])), book, "source_path")
                     title = " ".join(href.split("#")[1].split("-")).title()
@@ -74,7 +76,7 @@ def ref(matchobj):
                     logger.debug(f'Recursive title search result: {chapter["name"]}')
             except Exception as e:
                 logger.debug(e)
-                logger.error(f'Error getting chapter title: {path.normpath(path.join(dir,href))}')
+                logger.error(f'Error getting chapter title: {rel_path}')
                 sys.exit(1)
 
 
